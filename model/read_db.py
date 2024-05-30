8b355084a1ef319e1d77e8314775f635c2be5995
SheCode01 <shahera.ewaida@gmail.com>
2024-05-30 21:28:51 +0200

    fixed some errors

--- model/read_db.py
+++ model/read_db.py
@@ -82,7 +82,7 @@ class Read_db:
     
     def fetch_plans(self, user_id):
         self.open_db()
-        self.mycursor.execute(f"SELECT * FROM plans WHERE user_id = '{user_id}';")
+        self.mycursor.execute(f"SELECT * FROM to_do_list WHERE user_id = '{user_id}';")
         self.list_of_tuples = self.mycursor.fetchall()
     
         user_plans_list = []
