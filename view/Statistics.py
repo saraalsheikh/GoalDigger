ec733aa73a56e41f142d08d955cdfffc77f0d4ed
Nora <kafaji@gmail.com>
2024-05-30 18:18:34 +0200

    WIP

--- view/Statistics.py
+++ view/Statistics.py
@@ -1,37 +1,31 @@
-import sys
 import os
+import sys
+from PyQt5.QtWidgets import QMainWindow
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QVBoxLayout
 from PyQt5 import uic
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-
+from view.mood_tracker import MoodTrackerApp
 
 class StatisticsWindow(QMainWindow):
     def __init__(self, mood_data):
         super(StatisticsWindow, self).__init__()
         uic.loadUi('view/uifiles/statistics_window.ui', self)
+        
+        self.list_clicked = self.findChild(QListWidget, "listWidget")
 
-        # Example data: mood counts
-        self.mood_data = mood_data
-        self.plot_statistics()
+        self.list_clicked.connect()
+    
+    def get_mood(self):
+        moods = [1, 4, 3, 5, 2, 1, 5]
+        for mood in moods:
+            self.list_widget.addItem(str(mood))
 
-    def plot_statistics(self):
-        moods = list(self.mood_data.keys())
-        counts = list(self.mood_data.values())
-
-        # Create the figure and the bar chart
-        fig, ax = plt.subplots()
-        ax.bar(moods, counts, color=['green', 'gray', 'blue', 'red', 'orange'])
-        ax.set_ylabel('Count')
-        ax.set_title('Mood Statistics')
-
-        # Create a FigureCanvas and add it to the chartWidget
-        canvas = FigureCanvas(fig)
-        layout = QVBoxLayout(self.chartWidget)
-        layout.addWidget(canvas)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MoodTrackerApp()
+    mood_data = {"happy": 10, "neutral": 5, "sad": 7, "very sad": 3, "very happy": 12}
+    window = StatisticsWindow(mood_data)
     window.show()
     sys.exit(app.exec_())
